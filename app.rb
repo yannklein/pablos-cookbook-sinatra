efce0f3dbba56cd52f81f0dbab07e7be37359c4b
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-04-29 14:54:15 +0900

    setup

--- app.rb
+++ app.rb
@@ -2,16 +2,29 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+require "date"
+
+set :bind, "0.0.0.0"
 
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path(__dir__)
 end
 
-get "/" do  # <- Router part
-  "Hello world!"
-  # [...]   #
-  # [...]   # <- Controller part
-  # [...]   #
+get "/" do # <- router part
+  erb :index
+end
 
+get "/index" do # <- router part
+  erb :index
+end
+
+get "/about" do
+  erb :about
+end
+
+get "/team/:username" do
+  # binding.pry
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
